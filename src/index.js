6c367ec385778a9af1ebc615ad69880a602a38ce
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2026-04-16 16:37:26 +0200

    feat: initialize express server with health check and base API routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
+
 const productsRouter = require("./routes/products");
 const ordersRouter = require("./routes/orders");
 const usersRouter = require("./routes/users");
